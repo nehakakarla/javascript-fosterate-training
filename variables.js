60f8b873601c360a2a60535aa9c4b8cf96ba3412
Neha <nhkakarla@gmail.com>
2022-03-15 19:45:27 +0530

    Variables

---
+++ variables.js
@@ -0,0 +1,13 @@
+console.log("Variables");
+let x;  //declaring the variable
+x=5;  //assigning value
+let y=10;  //both declaring and assigning
+let z;
+console.log(x);
+console.log(y);
+//console.log(z); -- prints undefined
+x=15;
+console.log(x);
+const p=18;
+//p=20; -- defines error -- TypeError: Assignment to constant variable.
+console.log(p);
