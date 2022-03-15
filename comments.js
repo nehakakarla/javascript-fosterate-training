679927f5aed7e7b57baa58af2e9d563ad19c3790
Neha <nhkakarla@gmail.com>
2022-03-15 21:26:56 +0530

    Comments

---
+++ comments.js
@@ -0,0 +1,10 @@
+// -- single line comments
+/* -- multi-line comments */ 
+
+//Learning Javascript
+
+/*
+ JavaScript, often abbreviated JS, is a programming language.
+ It is one of the core technologies of the World Wide Web, alongside HTML and CSS. 
+ Over 97% of websites use JavaScript on the client side for web page behavior, often incorporating third-party libraries.
+*/
