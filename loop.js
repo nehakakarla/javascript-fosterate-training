ac14543a3917d69038ed84a6fcc8350ef4b79435
Neha <nhkakarla@gmail.com>
2022-03-16 18:44:11 +0530

    Looping Statements

---
+++ loop.js
@@ -0,0 +1,21 @@
+console.log("Loops");
+
+//for loop
+let sum=0;
+for(let i=1;i<=5;i++){
+    sum+=i;
+}
+console.log(sum);
+
+//while loop
+n=5;
+while(n>0){
+    console.log(n);
+    n--;
+}
+
+//do..while loop
+do{
+    console.log(n);
+    n++;
+}while(n<=5);
